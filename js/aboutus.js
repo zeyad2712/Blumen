7573e504820f8ba0cb547e84738f3241eeb48efc
Zeyad Waleed <zezowaleed2712@gmail.com>
2025-03-25 17:21:32 +0200

    Delete js directory

--- js/aboutus.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var h1Element = document.querySelector(".first h1");
-    if (h1Element) {
-        h1Element.classList.add("h1-animate");
-    }
-});
