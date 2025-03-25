b7c7552d2633a02b43e8e7ef0ffccfdb1322c50e
Zeyad Waleed <zezowaleed2712@gmail.com>
2025-03-25 22:26:54 +0200

    Add files via upload

---
+++ js/wishlist.js
@@ -0,0 +1,13 @@
+var card1 = document.getElementById('card1');
+var card2 = document.getElementById('card2');
+var card3 = document.getElementById('card3');
+
+function removeCard1() {
+    card1.style.display = 'none';
+}
+function removeCard2() {
+    card2.style.display = 'none';
+}
+function removeCard3() {
+    card3.style.display = 'none';
+}
