2aefabb60b90b6e3dc54e48e400ec3d6ac0abf96
Zeyad Waleed <zezowaleed2712@gmail.com>
2025-03-25 17:21:45 +0200

    Add files via upload

---
+++ js/aboutus.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var h1Element = document.querySelector(".first h1");
+    if (h1Element) {
+        h1Element.classList.add("h1-animate");
+    }
+});
